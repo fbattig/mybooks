0c98a0129a6d0e2a62a900b279cf3c3a5bf7ad70
Felix Battig <fbattig@yahoo.com>
2019-01-02 15:30:10 -0500

    Updates

---
+++ precache-manifest.11bd73bd5b2200d2d4064ea075dfd558.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/mybooks/static/media/arrow-drop-down.9d4e3f36.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/mybooks/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/mybooks/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "b60855e389ecb3e2e3f0",
+    "url": "/mybooks/static/js/runtime~main.b60855e3.js"
+  },
+  {
+    "revision": "bc797ff84e7642b03924",
+    "url": "/mybooks/static/js/main.bc797ff8.chunk.js"
+  },
+  {
+    "revision": "993b047e7a31f976e824",
+    "url": "/mybooks/static/js/1.993b047e.chunk.js"
+  },
+  {
+    "revision": "bc797ff84e7642b03924",
+    "url": "/mybooks/static/css/main.4110240f.chunk.css"
+  },
+  {
+    "revision": "b180373e66827c5f1c7ad4e0c18a80ca",
+    "url": "/mybooks/index.html"
+  }
+];
